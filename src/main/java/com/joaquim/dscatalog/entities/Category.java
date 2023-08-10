2f765cc2deef415b95587e51b16ea5ef6ab1f11c
Joaquim Ventura <joaquimventuradasilva@gmail.com>
2023-08-10 17:57:38 -0300

    First Commit

---
+++ src/main/java/com/joaquim/dscatalog/entities/Category.java
@@ -0,0 +1,47 @@
+package com.joaquim.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+
+    private Long id;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+}
